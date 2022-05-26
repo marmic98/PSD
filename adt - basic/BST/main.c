82ab9ebc1f82233ef1a108e7aba55686977eaaf2
marmic98 <michelmartino98.mm@gmail.com>
2022-05-26 17:57:10 +0200

    ex commit bst

--- adt - basic/BST/main.c
+++ adt - basic/BST/main.c
@@ -30,7 +30,7 @@ void printBST(BST b, int lv){
 }
 
 int kBalanced(BST b, int k){
-    return (abs(altezza(figlioSX(b)) - altezza(figlioDX(b))) < k);
+    return (abs(altezza(figlioSX(b)) - altezza(figlioDX(b))) <= k);
 }
 
 void printLeaves(BST b){
@@ -38,9 +38,9 @@ void printLeaves(BST b){
         return;
     if (emptyBST(figlioSX(b)) && emptyBST(figlioDX(b)))
         printItem(getItem(getRoot(b)));
+    
     printLeaves(figlioSX(b));
     printLeaves(figlioDX(b));
-
 }
 
 int isBothPresent(BST a, BST b){
@@ -53,18 +53,14 @@ int isBothPresent(BST a, BST b){
 }
 
 int isLeaf(BST b){
-    return emptyBST(figlioSX(b)) &&  emptyBST(figlioDX(b)); 
+    return (emptyBST(figlioSX(b)) && emptyBST(figlioDX(b))); 
 }
 
 int sumNodes(BST b){
-    int sum = 0;
-    if (isLeaf(b))
-        return getValue(getItem(getRoot(b)));
-    if(!emptyBST(figlioSX(b)))
-        sum += sumSubTree(figlioSX(b)) + getValue(getItem(getRoot(b)));
-    if(!emptyBST(figlioDX(b)))
-        sum += sumSubTree(figlioDX(b)) + getValue(getItem(getRoot(b)));
-    return sum;
+    if (emptyBST(b))
+        return 0;
+    else
+        return sumNodes(figlioDX(b)) + sumNodes(figlioSX(b)) + getValue(getItem(getRoot(b)));       
 }
 
 int sumSubTree(BST b){
@@ -85,20 +81,21 @@ int altezza(BST b){
 }
 
 int main(){
-    int a[7] = {0,1,2,3,4,5,6};
+    int a[8] = {0,1,2,3,0,1,2};
     int c[7] = {6,8,9,10,11,12,13};
     BST b1 = newBST();
     b1 = insertFromSortedArray(a, 0, 6);
     BST b2 = newBST();
     b2 = insertFromSortedArray(c, 0, 6);
     printBST(b1, 0);
-    int k = 1;
+    int k = 0;
     if (k == 0)
         printf("b is %s-balanced? %s\n", "perfect", kBalanced(b1, k) ? "si" : "no");   
     else 
         printf("b is %d-balanced? %s\n",k, kBalanced(b1, k) ? "si" : "no");
-    // printLeaves(b);
-    //printf("some of b1 is in b2? %s\n", isBothPresent(b1, b2) ? "si" : "no");    
-    printf("%d %d", sumNodes(figlioDX(b1)), sumNodes(figlioSX(b1)));
-    //printf("La somma dei valori dei sottoalberi e' la stessa? %s\n", sumSubTree(b1) ? "si" : "no");
+    printLeaves(b1);
+    printf("somma delle etichette di b1 = %d\n",sumNodes(b1));
+    //printf("some of b1 is in b2? %s\n", isBothPresent(b1, b2) ? "si" : "no"); 
+
+    printf("La somma dei valori dei sottoalberi e' la stessa? %s\n", sumSubTree(b1) ? "si" : "no");
 }
